899722e408b2a71b25d8909dba87435db755bd9a
YuriyBortnik <bortnikyuriy@gmail.com>
2019-12-22 12:46:34 +0100

    finish (Desktop)

---
+++ js/script.js
@@ -0,0 +1,12 @@
+burgerMenu.onclick = function myFunction() {
+    var x = document.getElementById("myMenu");
+
+    if(x.className === "menu") {
+        x.className += " responsive";
+    } else{
+        x.className = "menu";
+    }
+}
+
+
+$('.sl').slick();
