84ba3b1f6b06a42a4618830e9462d07758dcef72
YuriyBortnik <bortnikyuriy@gmail.com>
2019-12-25 09:38:10 +0100

    media 960-1200

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-burgerMenu.onclick = function myFunction() {
-    var x = document.getElementById("myMenu");
-
-    if(x.className === "menu") {
-        x.className += " responsive";
-    } else{
-        x.className = "menu";
-    }
-}
-
-
 $('.sl').slick();
+// burgerMenu.onclick = function myFunction() {
+//     var x = document.getElementById("myMenu");
+//
+//     if(x.className === "menu") {
+//         x.className += " responsive";
+//     } else{
+//         x.className = "menu";
+//     }
+// }
+
+
