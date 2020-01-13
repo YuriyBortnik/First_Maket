ad1436caa260a58085a58dca51f070566ac5f6c0
Yuriy <35668947+YuriyBortnik@users.noreply.github.com>
2020-01-13 17:16:32 +0100

    madia 1200-320 finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-$('.sl').slick();
+(function () {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger_active');
+    });
+}());
 
 menu.onclick = function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -8,4 +13,6 @@ menu.onclick = function myFunction() {
     } else {
         x.className = "topnav";
     }
-}
+};
+
+$('.sl').slick();
