6c30b4814b0adbd0ebc0dfe9843cd70053173b33
Yuriy <35668947+YuriyBortnik@users.noreply.github.com>
2020-01-17 19:15:48 +0100

    online

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@
 
 }());
 
-menu.onclick = function myFunction() {
-    var x = document.getElementById("myTopnav");
+// menu.onclick = function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// };
 
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-};
-
-$('.sl').slick();
+// $('.sl').slick();
